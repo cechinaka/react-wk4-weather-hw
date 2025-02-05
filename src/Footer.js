27ea5eb6965a931f5cdbf54fa2f909184b51d56c
cechinaka <chinaka08@gmail.com>
2025-02-04 17:56:46 -1000

    updated URL Footer links

--- src/Footer.js
+++ src/Footer.js
@@ -5,18 +5,18 @@ export default function Footer() {
     <div>
       <footer>
         This project was coded by
-        <a href="https://github.com/cechinaka" target="_blank">
+        <a href="https://github.com/cechinaka" rel="noreferrer" target="_blank">
           Christie Chinaka
         </a>
         and is on
         <a
           href="https://github.com/cechinaka/react-wk4-weather-hw"
-          target="_blank"
+          rel="noreferrer" target="_blank"
         >
           GitHub
         </a>
         and hosted on
-        <a href="https://fantastic-alpaca-db7190.netlify.app/" target="_blank">
+        <a href="https://fantastic-alpaca-db7190.netlify.app/" rel="noreferrer" target="_blank">
           Netlify.
         </a>
       </footer>
